a13ca4579b5ab32a214019cd162a4afabfe0a074
Jah <tess9roses@gmail.com>
2017-12-05 21:16:10 +0300

    Now it's really doing something...

--- core.js
+++ core.js
@@ -1,22 +1,78 @@
 const https = require('https');
+//const fs = require('fs');
 
-function getCert(url) {
+let warnCount = 0,
+errCount = 0;
+
+function getCertificateInfo(url) {
     let options = {
         host: url,
-        method: 'GET',
-    }
+        method: 'GET'
+    };
 
-    let req = https.request( options, function(res) {
-        console.log( url + ' - ' + res.connection.getPeerCertificate().issuer.O );
-    }).on( 'error', function(err) {
-        console.log( url + ' - No certificate' );
+    var req = https.request(options, function(res) {
+    let siteCert = res.connection.getPeerCertificate(true).issuer.O,
+    body = '';
+
+    res.on("data", function(chunk) {
+        body += chunk.toString(); //combine all cnunks in one
+    }).on("end", function() {
+        //console.log('Cert - ' + url + ' - ' + siteCert);
+
+        const src = /src="http:/;
+        const href = /href="http:/;
+
+        let isSrc = src.test(body),
+        isHref = href.test(body);
+
+        if ( isSrc && !isHref ) {
+            console.log('\x1b[31mError\x1b[0m - ' + url + ' - Mixed content detected');
+            errCount++;
+        }
+
+        if ( !isSrc && isHref ) {
+            console.log('\x1b[33mWarn\x1b[0m - ' + url + ' - HTTP links detected');
+            warnCount++;
+        }
+
+        if ( isSrc && isHref) {
+            console.log('\x1b[31mError\x1b[0m/\x1b[33mWarn\x1b[0m - ' + url + ' - Mixed content and HTTP links detected');
+            warnCount++;
+            errCount++;
+        }
+
+        if ( !isSrc && !isHref ) {
+            console.log('\x1b[32mOK\x1b[0m - ' + url);
+        }
+
+        /*
+        fs.writeFile("body.txt", body, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log('\x1b[32mfile created\x1b[0m');
+        });
+        */
+
+        if ( params.length != 0 ) {
+            getCertificateInfo(params.shift())
+        } else {
+            console.log('Work\'s done with: ' + errCount + ' error(s) and ' + warnCount + ' warning(s)');
+            }
+    });
+
+    }).on('error', function(err) {
+        console.log('No certificate found - ' + url);
     });
 
     req.end();
 }
 
-let params = process.argv[2].split(',');
+let params = [];
 
-for ( let i = 0; i < params.length; i++ ) {
-    getCert( params[i] );
-};
+for ( let i = 2; i < process.argv.length; i++) {
+    params.push(process.argv[i].replace(/,/g, ''));
+}
+console.log('Url\'s number: ' + params.length)
+
+getCertificateInfo(params.shift());
