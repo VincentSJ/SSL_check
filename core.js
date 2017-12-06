d05a2dc20ff5e527424e512a66eada7ad6b7f13a
Jah <jah@localhost.localdomain>
2017-12-06 07:27:02 -0500

    more accurate regexp

--- core.js
+++ core.js
@@ -11,39 +11,39 @@ function getCertificateInfo(url) {
     };
 
     let req = https.request(options, function(res) {
-    let siteCert = res.connection.getPeerCertificate(true).issuer.O,
-        body = '';
+        let siteCert = res.connection.getPeerCertificate(true).issuer.O,
+            body = '';
 
-    res.on("data", function(chunk) {
-        body += chunk.toString(); //combine all cnunks in one
-    }).on("end", function() {
-        //console.log('Cert - ' + url + ' - ' + siteCert);
+        res.on("data", function(chunk) {
+            body += chunk.toString(); //combine all cnunks in one
+        }).on("end", function() {
+            //console.log('Cert - ' + url + ' - ' + siteCert);
 
-        const src = /src="http:/,
-              href = /href="http:/;
+            const src = /src="http:\/\/[^"']/;
+                  href = /href="http:\/\/(?!it-clever\.ru)/;
 
-        let isSrc = src.test(body),
-            isHref = href.test(body);
+            let isSrc = src.test(body),
+                isHref = href.test(body);
 
-        if ( isSrc && !isHref ) {
-            console.log('\x1b[31mError\x1b[0m - ' + url + ' - Mixed content detected');
-            errCount++;
-        }
+            if ( isSrc && !isHref ) {
+                console.log('\x1b[31mError\x1b[0m - ' + url + ' - Mixed content detected');
+                errCount++;
+            }
 
-        if ( !isSrc && isHref ) {
-            console.log('\x1b[33mWarn\x1b[0m - ' + url + ' - HTTP links detected');
-            warnCount++;
-        }
+            if ( !isSrc && isHref ) {
+                console.log('\x1b[33mWarn\x1b[0m - ' + url + ' - HTTP links detected');
+                warnCount++;
+            }
 
-        if ( isSrc && isHref) {
-            console.log('\x1b[31mError\x1b[0m/\x1b[33mWarn\x1b[0m - ' + url + ' - Mixed content and HTTP links detected');
-            warnCount++;
-            errCount++;
-        }
+            if ( isSrc && isHref) {
+                console.log('\x1b[31mError\x1b[0m/\x1b[33mWarn\x1b[0m - ' + url + ' - Mixed content and HTTP links detected');
+                warnCount++;
+                errCount++;
+            }
 
-        if ( !isSrc && !isHref ) {
-            console.log('\x1b[32mOK\x1b[0m - ' + url);
-        }
+            if ( !isSrc && !isHref ) {
+                console.log('\x1b[32mOK\x1b[0m - ' + url);
+            }
 
         /*
         fs.writeFile("body.txt", body, function(err) {
@@ -54,12 +54,12 @@ function getCertificateInfo(url) {
         });
         */
 
-        if ( params.length != 0 ) {
-            getCertificateInfo(params.shift())
-        } else {
-            console.log('Work\'s done with: ' + errCount + ' error(s) and ' + warnCount + ' warning(s)');
-        }
-    });
+            if ( params.length != 0 ) {
+                getCertificateInfo(params.shift())
+            } else {
+                console.log('Work\'s done with: ' + errCount + ' error(s) and ' + warnCount + ' warning(s)');
+            }
+        });
 
     }).on('error', function(err) {
         console.log('No certificate found - ' + url);
